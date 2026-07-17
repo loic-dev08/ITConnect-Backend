577bda2c3791408f6f50c9b2d5068c646c4ebb19
Loic <leotetloic08@gmail.com>
2026-07-17 07:24:01 +0200

    amélioration et mise à jour de entrepriseController.js

--- routes/entreprises.js
+++ routes/entreprises.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const router  = express.Router()
-const { getEntreprises } = require('../controllers/entrepriseController')
-const express = require('express')
-const router  = express.Router()
 const { getEntreprises, getEntrepriseById } = require('../controllers/entrepriseController')
 
-router.get('/', getEntreprises)
+router.get('/',    getEntreprises)
 router.get('/:id', getEntrepriseById)
-
-module.exports = router
-
-router.get('/', getEntreprises)
 
 module.exports = router
